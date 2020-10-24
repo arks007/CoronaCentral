703964a2032d02f93b5535f852d1ffa261d695b8
Sujoy Purkayastha <sujoy2900@gmail.com>
2020-10-24 13:40:41 -0500

    Initial Commit

---
+++ CoronaCentral/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoronaCentral
+//
+//  Created by Sujoy Purkayastha on 10/24/20.
+//  Copyright © 2020 Sujoy Purkayastha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
